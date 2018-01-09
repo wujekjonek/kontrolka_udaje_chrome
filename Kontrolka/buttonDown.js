69899daf84ec3614973bf63338b737a88c1a47f1
jonasz.kozlowski <termometr@poczta.onet.pl>
2018-01-09 09:14:28 +0100

    a

--- Kontrolka/buttonDown.js
+++ Kontrolka/buttonDown.js
@@ -3,17 +3,21 @@ function buttonDown() {
     // todo tu kod z arrow down
     let poleGodzin = document.getElementById("godzina");
     let tymczasowaZmienna = parseInt(poleGodzin.value) - 1;
+
     if (tymczasowaZmienna < 10 && tymczasowaZmienna >= 0) {
         tymczasowaZmienna = "0" + tymczasowaZmienna;
     }
+
     if (tymczasowaZmienna === -1) {
         console.log("tymczasowaZmienna == -1   ");
         tymczasowaZmienna = 23;
     }
+
     if (poleGodzin.value === "") {
         console.log("tymczasowaZmienna == -1   ");
         tymczasowaZmienna = 23;
     }
+
     poleGodzin.value = tymczasowaZmienna;
     pokazIks();
 
--- Kontrolka/focusMinuta.js
+++ Kontrolka/focusMinuta.js
@@ -1,10 +1,6 @@
 function focusMinuta() {
     document.getElementById("minuta").style.backgroundColor = "lightblue";
-    console.log(this);
-
-
-
-
+    // console.log(this);
 
     console.log("function focusMinuta() ***********************");
 
@@ -16,10 +12,9 @@ function focusMinuta() {
 function focusThis() {
     this.style.backgroundColor = "lightblue";
     console.log(this);
-
     let activeElement = this;
 
-
-
-
 }
+
+
+
